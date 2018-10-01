13cb23d0d4407f9556c19aa877fdf3e0ff22a428
Diaz <david.stiven.diaz@accenture.com>
2018-10-01 14:23:57 -0500

    Añadimos vistas para mostrar artistas

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,7 +1,9 @@
-class ArtistController < ApplicationController
+class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 end
